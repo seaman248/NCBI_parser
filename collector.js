d8bc01fa7d28918d3902f388253449a83b923ece
seaman248 <semen.bko@ya.ru>
2014-12-20 11:56:24 +0300

    +collector

---
+++ collector.js
@@ -0,0 +1,23 @@
+var getAbstract = require('./abstractor.js'),
+	generator = require('./generator.js'),
+	fs = require('fs');
+
+
+// getAbstract('cell', function (err, abstract) {
+// 	if (err) console.log(err);
+// 	console.log(abstract); 
+// });
+
+// generator('inputText.txt', function (wordArr) {
+// 	console.log(wordArr);
+// });
+generator('inputText.txt', function (err, searchWords) {
+	for (var i = 100; i < 200; i++) {
+		getAbstract(searchWords[i], function (err, abstract) {
+			fs.appendFile('output.txt', abstract ,function (err) {
+				if (err) console.log(err);
+				console.log('OK');
+			})
+		})
+	};
+});
